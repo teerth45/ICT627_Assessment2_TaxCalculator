a5d1f0c9aa019788f32d4934ea572f1dd3ccf492
Rheginald GREGORIO <rheginald.gregorio@gmail.com>
2021-09-06 09:37:26 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ICT627_Assessment2_TaxCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
